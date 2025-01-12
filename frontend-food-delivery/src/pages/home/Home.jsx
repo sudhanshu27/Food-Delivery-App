86e9656579cf600774ac1343b335511185257a67
Sudhanshu Pal <palsudhanshu177@gmail.com>
2025-01-12 14:15:25 +0530

    new commit

--- frontend-food-delivery/src/pages/home/Home.jsx
+++ frontend-food-delivery/src/pages/home/Home.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import "./Home.css";
 import Header from "../../components/header/Header";
-import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
-import FoodDisplay from "../../components/foodDisplay/FoodDisplay";
-import AppDownload from "../../components/appDownload/AppDownload";
+import ExploreMenu from "../../components/ExploreMenu/ExploreMenu.jsx";
+import FoodDisplay from "../../components/foodDisplay/FoodDisplay.jsx";
+import AppDownload from "../../components/appDownload/AppDownload.jsx";
 
 const Home = () => {
   const [category, setCategory] = useState("All");
